37753bc7464b1b3ce473a48830ac1b4d20888c7f
samoculus <samedkaragoz11@gmail.com>
2022-12-06 07:44:20 -0500

    improvements

--- contracts/GatekeepBrink.sol
+++ contracts/GatekeepBrink.sol
@@ -1,43 +1,37 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract GatekeepBrink is Ownable, AccessControl  {
-    address public gatekeepGuard = 0xb265251f1084397bdBb434b932a6070220F3784A;
+    using SafeERC20 for IERC20;
+
     address public userAddress;
     address public safeAddress;
-    address public brinkAddress;
 
     bytes32 public constant GUARD_ROLE = keccak256("GUARD_ROLE");
 
     constructor() {
-        _grantRole(GUARD_ROLE, gatekeepGuard);
+        _grantRole(GUARD_ROLE, 0xb265251f1084397bdBb434b932a6070220F3784A);
         _grantRole(GUARD_ROLE, msg.sender);
         userAddress = msg.sender;
         safeAddress = msg.sender;
-        brinkAddress = address(this);
-    }
-
-    receive() external payable {}
-
-    fallback() external payable {}
-
-    function getBalance() public view returns (uint) {
-        return address(this).balance;
     }
 
     function setUserAddress(address userAddress_ ) external onlyOwner {
         // Set wallet address to be actively monitored.
+        require(userAddress_ != address(0), "Invalid address");
         userAddress = userAddress_;
     }
 
     function setSafeAddress(address safeAddress_ ) external onlyOwner {
         // Set address ( cold wallet recommended ) that will act as a vault. Assets will be transferred here.
+        require(safeAddress_ != address(0), "Invalid address");
         safeAddress = safeAddress_;
     }
 
@@ -50,11 +44,15 @@ contract GatekeepBrink is Ownable, AccessControl  {
     }
 
     function intercept721(uint256 _tokenId, address _contractAddress) external onlyRole(GUARD_ROLE) {
+        require(_contractAddress != address(0), "Invalid address");
+
         // Intercept ERC721 asset transfer
         ERC721(_contractAddress).safeTransferFrom(userAddress, safeAddress, _tokenId);
     }
 
     function intercept1155(uint256 _tokenId, address _contractAddress) external onlyRole(GUARD_ROLE) {
+        require(_contractAddress != address(0), "Invalid address");
+
         // Intercept ERC1155 asset transfer
         ERC1155 token1155 = ERC1155(_contractAddress);
 
@@ -66,11 +64,13 @@ contract GatekeepBrink is Ownable, AccessControl  {
     }
 
     function intercept20(address _contractAddress) external onlyRole(GUARD_ROLE) {
+        require(_contractAddress != address(0), "Invalid address");
+        
         // Intercept ERC20 asset transfer
-        ERC20 token20 = ERC20(_contractAddress);
+        IERC20 token20 = IERC20(_contractAddress);
 
         uint256 totalBalance20 = token20.balanceOf(userAddress);
 
-        token20.transferFrom(userAddress, safeAddress, totalBalance20);
+        token20.safeTransferFrom(userAddress, safeAddress, totalBalance20);
     }
 }
